488e7345079f6f8195950aec1b6024a535cacea7
animevn <haanhth21@gmail.com>
2019-10-17 00:31:33 +0700

    Enable loader for one button, the other will be the same. Need to try to get the action survive activity reset

--- app/src/main/java/com/haanhgs/asyncloadermultithreaddemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/asyncloadermultithreaddemo/MainActivity.java
@@ -1,13 +1,110 @@
 package com.haanhgs.asyncloadermultithreaddemo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.text.MessageFormat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Integer> {
+
+    private Loader<Integer> loader1;
+    private Button bnThread1;
+    private ProgressBar pbrThread1;
+    private TextView tvThread1;
+
+
+    private void initViews(){
+        bnThread1 = findViewById(R.id.bnThread1);
+        pbrThread1 = findViewById(R.id.pbrThread1);
+        tvThread1 = findViewById(R.id.tvThread1);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (LoaderManager.getInstance(this).getLoader(1) != null){
+            LoaderManager.getInstance(this).initLoader(1, null, this);
+        }
+
+        if (savedInstanceState != null){
+            ((ProgressBar)findViewById(R.id.pbrThread1)).setProgress(savedInstanceState.getInt(
+                    "pr1"));
+            findViewById(R.id.bnThread1).setEnabled(savedInstanceState.getBoolean("bn1"));
+        }
+
+        initViews();
+
+        bnThread1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putInt("thread1", 500);
+                LoaderManager.getInstance(MainActivity.this).restartLoader(1, bundle,
+                        MainActivity.this);
+                tvThread1.setText(R.string.tvThreadRunning);
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("pr1", ((ProgressBar)findViewById(R.id.pbrThread1)).getProgress());
+        outState.putBoolean("bn1", (findViewById(R.id.bnThread1)).isEnabled());
+    }
+
+    @NonNull
+    @Override
+    public Loader<Integer> onCreateLoader(int id, @Nullable Bundle args) {
+        bnThread1.setEnabled(false);
+        int seed = 0;
+        if (args != null){
+            seed = args.getInt("thread1");
+        }
+        loader1 = new SimpleLoader(this);
+        ((SimpleLoader)loader1).setSeed(seed);
+        ((SimpleLoader)loader1).setListener(new SimpleLoader.Listener() {
+            @Override
+            public void onMaxCreate(int duration) {
+                pbrThread1.setMax(duration);
+                pbrThread1.setProgress(0);
+            }
+
+            @Override
+            public void onDataChanged(int progress) {
+                pbrThread1.setProgress(progress);
+            }
+        });
+
+        return loader1;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Integer> loader, Integer data) {
+        if (loader == loader1){
+            bnThread1.setEnabled(true);
+            tvThread1.setText(MessageFormat.format("The thread " +
+                    "finishes " +
+                    "in " +
+                    "{0} " +
+                    "milisecs", data));
+            LoaderManager.getInstance(this).destroyLoader(1);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Integer> loader) {
+        Toast.makeText(this, "reset", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/haanhgs/asyncloadermultithreaddemo/SimpleLoader.java
+++ app/src/main/java/com/haanhgs/asyncloadermultithreaddemo/SimpleLoader.java
@@ -1,25 +1,56 @@
 package com.haanhgs.asyncloadermultithreaddemo;
 
 import android.content.Context;
+import java.util.Random;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.loader.content.AsyncTaskLoader;
 
-public class SimpleLoader extends AsyncTaskLoader<Integer> {
+public class SimpleLoader extends AsyncTaskLoader<Integer>{
+
+    interface Listener{
+        void onMaxCreate(int duration);
+        void onDataChanged(int progress);
+    }
+
+    private int seed;
+    private int duration;
+    private Listener listener;
 
     public SimpleLoader(@NonNull Context context) {
         super(context);
     }
 
+    public void setSeed(int seed) {
+        this.seed = seed;
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
     @Override
     protected void onStartLoading() {
         super.onStartLoading();
+        Random random = new Random();
+        duration = (random.nextInt(10) + 1) * seed;
+        listener.onMaxCreate(duration);
         forceLoad();
     }
 
     @Nullable
     @Override
     public Integer loadInBackground() {
-        return null;
+        int count = 0;
+        while (count < duration){
+            try{
+                Thread.sleep(duration/10);
+                count += duration/10;
+                listener.onDataChanged(count);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        return duration;
     }
 }
