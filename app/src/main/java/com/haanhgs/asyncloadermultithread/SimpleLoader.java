4188ba5e8a2189b5c298c68086fa14fc047bc801
animevn <haanhth21@gmail.com>
2020-01-07 10:22:34 +0700

    add loader

---
+++ app/src/main/java/com/haanhgs/asyncloadermultithread/SimpleLoader.java
@@ -0,0 +1,47 @@
+package com.haanhgs.asyncloadermultithread;
+
+import android.content.Context;
+import android.widget.ProgressBar;
+import java.lang.ref.WeakReference;
+import java.util.Random;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+public class SimpleLoader extends AsyncTaskLoader<Integer> {
+
+    private final WeakReference<ProgressBar> pbr;
+    private final int seed;
+
+
+    public SimpleLoader(@NonNull Context context, ProgressBar pbr, int seed) {
+        super(context);
+        this.seed = seed;
+        this.pbr = new WeakReference<>(pbr);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        forceLoad();
+    }
+
+    @Nullable
+    @Override
+    public Integer loadInBackground() {
+        Random random = new Random();
+        int s = (random.nextInt(10) + 1) * seed;
+        pbr.get().setMax(s);
+        int count = 0;
+        while (count < s){
+            try{
+                Thread.sleep(s/10);
+                count += s/10;
+                pbr.get().setProgress(count);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        return s;
+    }
+}
